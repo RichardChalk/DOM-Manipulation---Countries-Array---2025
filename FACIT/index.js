e9d3bc11bb7c6b1793923c15f6a9780ce0735b97
Richard Chalk <barmybrit@gmail.com>
2025-12-04 06:36:24 +0100

    first commit

---
+++ FACIT/index.js
@@ -0,0 +1,57 @@
+// Use the countries array to display all the countries.
+
+// OLD!!!!!
+// const countriesAPI = "https://restcountries.com/v2/all";
+const countriesAPI = "https://restcountries.com/v3.1/independent?status=true";
+
+let totalCountries = 0;
+
+const showCountries = async () => {
+  try {
+    const response = await fetch(countriesAPI);
+    data = await response.json();
+    totalCountries = data.length;
+    showNumberOfCountries(totalCountries);
+    showData(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+function showData(data) {
+  let countryContainer = document.createElement("div");
+  countryContainer.className = "country-container";
+  document.body.appendChild(countryContainer);
+
+  data.forEach((country) => {
+    const countryText = document.createElement("div");
+    // OLD!!!!!
+    // countryText.textContent = `${country.name}`;
+    countryText.textContent = country.name.common; // v3-format
+    countryText.className = "country-text";
+    countryContainer.appendChild(countryText);
+  });
+}
+
+function showTitle() {
+  let title = document.createElement("div");
+  title.textContent = "World Countries List";
+  title.style.fontSize = "40px";
+  title.style.textAlign = "center";
+  title.style.fontWeight = "bold";
+  title.style.marginTop = "40px";
+  document.body.appendChild(title);
+}
+
+function showNumberOfCountries(totalCountries) {
+  let numberOfCountries = document.createElement("div");
+  numberOfCountries.textContent = `Total number of countries: ${totalCountries}`;
+  numberOfCountries.style.fontSize = "30px";
+  numberOfCountries.style.textAlign = "center";
+  numberOfCountries.style.fontWeight = "bold";
+  numberOfCountries.style.marginTop = "10px";
+  document.body.appendChild(numberOfCountries);
+}
+
+showTitle();
+showCountries();
---
+++ index.js
@@ -0,0 +1,7 @@
+// Use the countries array to display all the countries.
+// Titta på bilden "Exempel.png" för att få veta ungefär hur resultatet ska se ut.
+
+// Dessa två rader får ni som ledtråd på hur ni ska börja.
+const countriesAPI = "https://restcountries.com/v2/all";
+let totalCountries = 0;
+
